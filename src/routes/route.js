72797b5c6a8ebfd6568bb42ec96aba7855ef9065
unbeatable-tech <aliyadav950@gmail.com>
2022-04-07 23:07:39 +0530

    soltion of count

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,13 +3,7 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
-});
+
 let arr=["amit","kanishq","akhil","shivam","ashish","rahul","harish","vineet","sahil","rohan"]
 
 router.get('/all-candidates',function (req,res)
