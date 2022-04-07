a227e7cc02c92e8b01d496caaab5b1b54b85f8e9
unbeatable-tech <aliyadav950@gmail.com>
2022-04-08 01:43:36 +0530

    THE SOLUTION OF API

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,32 +3,29 @@ const logger = require('./logger')
 
 const router = express.Router();
 
+let movie=["rang de basanti","RRR","the kashmir files","the shinning","lord of rings","sholay"]
 
-let movie=["rand de basnasti", "the shining", "lord of the rings", "bartman begins","The kashmir files","RRR"]
 router.get('/movies',function(req,res){
     res.send(movie)
 })
 
 router.get('/movies/:indexNumber',function(req,res){
-    let values=req.params.indexNumber
-    let result=movie[values]
+let values=req.params.indexNumber
+let result=movie[values]
 
-    if(values>movie.length){
-        res.send("use a valid index")
-    }
-else
-    
-   {
+if(values>movie.length){
+    res.send("use a valid index")
+}
+
+else{
     res.send(result)
-   }
+}
 
 
 })
 
-
-
-   router.get('/films',function(req,res){
-    let film=[ {
+router.get('/films',function(req,res){
+let film=    [ {
         "id": 1,
         "name": "The Shining"
        }, {
@@ -41,11 +38,13 @@ else
         "id": 4,
         "name": "Finding Nemo"
        }]
+       
 res.send(film)
+    
+})
 
-   })
-   router.get('/films/:filmId',function(req,res){
-    let film=[ {
+router.get('/films/:filmId',function(req,res){
+    let film=    [ {
         "id": 1,
         "name": "The Shining"
        }, {
@@ -59,22 +58,24 @@ res.send(film)
         "name": "Finding Nemo"
        }]
 
-let value=req.params.filmId
-let no =0
-for(let i=0;i<film.length;i++){
-    if(film[i].id==value){
-        res.send(film[i])
-        no =1
-        break
-    }
-}
+      
+       let value= req.params.filmId
+       let no =0
+       for(let i=0;i<film.length;i++){
+           if(value==film[i].id){
+               res.send(film[i])
+               no=1
+               break
+           }
+       }
 
-if(no==0){
-    res.send(" No movie exists with this id.")
-}
+       if(no ==0){
+           res.send("No movie exists with this id")
 
+           
+       }
 
-   })
+})
    
 
 module.exports = router;
